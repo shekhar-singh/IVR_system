3230be8d00f4ae1c9dea4f47402eedc00a13f495
Shekhar Singh Choudhary <sschoudhary60@gmail.com>
2019-08-02 15:53:28 +0530

    static root conflict fix

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -137,14 +137,21 @@ CELERY_TIMEZONE = 'Asia/Kolkata'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+# STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR, 'contacts/static/contacts/static'),
+#   os.path.join(BASE_DIR, 'userauth/static/')
+# )
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# Add these new lines
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'contacts/static/contacts/static'),
-  os.path.join(BASE_DIR, 'userauth/static/')
+    os.path.join(BASE_DIR, 'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/user/'
